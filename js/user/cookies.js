e9e36b204cfd75180c2f9ead14147995bb2efd21
Pablo03-byte <pbc0017@alu.medac.es>
2025-05-15 11:09:16 +0200

    Cambios configuración cookies

--- js/user/cookies.js
+++ js/user/cookies.js
@@ -1,8 +1,32 @@
-function acceptCookies() {
+/*function acceptCookies() {
   document.getElementById('cookie-banner').style.display = 'none';
 }
 
 function rejectCookies() {
   document.getElementById('cookie-banner').style.display = 'none';
 }
+*/
+// Mostrar el banner solo si no hay decisión guardada en sessionStorage
+window.addEventListener('DOMContentLoaded', function () {
+  if (!sessionStorage.getItem('cookies-decision')) {
+    document.getElementById('cookie-banner').style.display = 'block';
+  }
+});
 
+function acceptCookies() {
+  sessionStorage.setItem('cookies-decision', 'aceptadas');
+  document.getElementById('cookie-banner').style.display = 'none';
+}
+
+function rejectCookies() {
+  sessionStorage.setItem('cookies-decision', 'rechazadas');
+  document.getElementById('cookie-banner').style.display = 'none';
+}
+
+
+window.addEventListener("load", () => {
+  if (!sessionStorage.getItem("cookies-popupMostrado")) {
+    document.getElementById("cookie-banner").style.display = "block";
+    sessionStorage.setItem("cookies-popupMostrado", "true");
+  }
+});

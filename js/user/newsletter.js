4b70a8b9ff9b18823e0559d3c5aaa74232f80892
Pablo03-byte <pbc0017@alu.medac.es>
2025-05-15 09:59:44 +0200

    copia de seguridad antes de romperlo

--- js/user/newsletter.js
+++ js/user/newsletter.js
@@ -1,11 +1,18 @@
 function closeNewsletterPopup() {
   document.getElementById('newsletter-popup').style.display = 'none';
+  document.getElementById("newsletter-popup-overlay").style.display = "none";
 }
 
 // Mostrar el pop-up al cargar la página
-window.onload = function() {
-  document.getElementById('newsletter-popup').style.display = 'flex';
-};
+
+window.addEventListener("load",  function() {
+    if (!sessionStorage.getItem("newsletter-popupMostrado")) {
+            setTimeout(() => {
+              document.getElementById("newsletter-popup-overlay").style.display = "flex";
+              sessionStorage.setItem("newsletter-popupMostrado", "true"); // Marquem que ja s’ha mostrat
+              }, 2000);
+    };
+} );
 
 // Mostrar mensaje de éxito al suscribirse
 document.addEventListener('DOMContentLoaded', function() {
